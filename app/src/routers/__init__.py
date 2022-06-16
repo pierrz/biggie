a3b73565df07a5ca3bc2496adeb57bb291c96b0b
Pierre-Edouard Barrault <pierrz@tuta.io>
2022-06-16 22:00:17 +0200

    removed duplicated code ??

--- app/src/routers/__init__.py
+++
@@ -1,3 +0,0 @@
-from fastapi.templating import Jinja2Templates as jinja
-
-templates = jinja(directory="templates")
--- app/src/routers/router_base.py
+++
@@ -1,7 +0,0 @@
-"""
-Module gathering the Jinja2 templates
-"""
-
-from fastapi.templating import Jinja2Templates
-
-templates = Jinja2Templates(directory="templates")
