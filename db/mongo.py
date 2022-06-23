381d94bb60b7c7b88e4a922d4e56f98df3deb811
Pierre-Edouard Barrault <pierrz@tuta.io>
2022-06-23 22:26:24 +0200

    env move

--- db/mongo.py
+++ db/mongo.py
@@ -7,14 +7,4 @@ import os
 from pymongo import MongoClient
 
 mongo = MongoClient(os.getenv("MONGODB_URI"))
-# db = mongo[os.getenv("DB_NAME")]
-
-
-def getdb():
-    print(os.getenv("DB_NAME"))
-    print(os.getenv("MONGODB_URI"))
-    print(os.getenv("MONGO_INITDB_ROOT_USERNAME"))
-    return mongo[os.getenv("DB_NAME")]
-
-
-db = getdb()
+db = mongo[os.getenv("DB_NAME")]
