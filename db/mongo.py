b6698bc17e178f417f3ba9c93776a262a600c31d
Pierre-Edouard Barrault <pierrz@tuta.io>
2022-06-23 22:17:20 +0200

    more check

--- db/mongo.py
+++ db/mongo.py
@@ -7,4 +7,12 @@ import os
 from pymongo import MongoClient
 
 mongo = MongoClient(os.getenv("MONGODB_URI"))
-db = mongo[os.getenv("DB_NAME")]
+# db = mongo[os.getenv("DB_NAME")]
+
+
+def getdb():
+    print(os.getenv("DB_NAME"))
+    return mongo[os.getenv("DB_NAME")]
+
+
+db = getdb()
