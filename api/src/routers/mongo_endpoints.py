7c81575b337d4d670481e5a8823a85b93dd14722
Pierre-Edouard Barrault <pierrz@tuta.io>
2022-11-12 03:11:23 +0100

    checked with live data

--- api/src/routers/mongo_endpoints.py
+++ api/src/routers/mongo_endpoints.py
@@ -26,13 +26,8 @@ class RepoList(BaseModel):
 @router.get("/aggregated_repo_list")
 async def aggregated_repo_list():
     mongodb = init_mongo_connection()  # pylint: disable=C0103
-    db_data = mongodb.event.aggregate(
-        [
-            {"$group": {"_id": "$repo_name", "count": {"$count": {}}}},
-            {"$sort": {"count": -1}},
-        ]
-    )
-    results_df = dataframe_from_mongo_data(db_data, sort_by="count")
+    db_data = mongodb.event.aggregate([{"$sortByCount": "$repo_name"}])
+    results_df = dataframe_from_mongo_data(db_data)
     return RepoList(
         repository_list=[
             Repo(name=repo["_id"], count=repo["count"])
