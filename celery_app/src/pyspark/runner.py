2e5fdb1d635c407629187d35a4a529aa3016a005
Pierre-Edouard Barrault <pierrz@tuta.io>
2024-08-21 16:32:30 +0200

    updated mongo connector; hack to bypass the missing 'inferred' metadata issue

--- celery_app/src/pyspark/runner.py
+++ celery_app/src/pyspark/runner.py
@@ -5,15 +5,15 @@ Spark core runner
 from config import pyspark_config
 from pyspark.sql import SparkSession  # pylint: disable=E0611
 
-# TODO: update connector version
 spark_mongo = (
     SparkSession.builder.appName("Spark/Mongo IO")
     .master("local[2]")
     .config("spark.executor.memory", "2g")
-    .config("spark.mongodb.input.uri", pyspark_config.MONGODB_URI)
-    .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:3.0.0")
+    .config("spark.mongodb.connection.uri", pyspark_config.MONGODB_URI)
+    .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:10.4.0")
     .getOrCreate()
 )
+
 spark_postgres = (
     SparkSession.builder.appName("Spark/Postgres IO")
     .master("local[2]")
@@ -22,23 +22,7 @@ spark_postgres = (
     .config("spark.driver.extraClassPath", "/opt/spark/jars/postgresql-42.7.3.jar")
     .getOrCreate()
 )
-# spark_mongo = (
-#     SparkSession.builder.appName("Spark/Mongo IO")
-#     .master("local[2]")
-#     .config("spark.executor.memory", "2g")
-#     .config("spark.mongodb.input.uri", pyspark_config.MONGODB_URI)
-#     .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:3.0.0")
-#     .getOrCreate()
-# )
-#
-# spark_postgres = (
-#     SparkSession.builder.appName("Spark/Postgres IO")
-#     .master("local[2]")
-#     .config("spark.executor.memory", "2g")
-#     .config("spark.executor.extraClassPath", "/opt/spark/jars/postgresql-42.4.1.jar")
-#     .config("spark.driver.extraClassPath", "/opt/spark/jars/postgresql-42.4.1.jar")
-#     .getOrCreate()
-# )
+
 
 # cheatsheet for further settings
 # .config("spark.mongodb.input.uri", "mongodb://mongo1:27017,mongo2:27018,mongo3:27019/Stocks.Source?replicaSet=rs0")\
