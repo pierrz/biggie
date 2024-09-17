39637162475697302d8cf60ada534fbe7ace503e
Pierre-Edouard Barrault <pierrz@tuta.io>
2024-09-17 12:06:56 +0300

    mongo test passed

--- celery_app/src/sparky/runner.py
+++ celery_app/src/sparky/runner.py
@@ -6,32 +6,55 @@ from config import pyspark_config
 from pyspark.sql import SparkSession  # pylint: disable=E0611
 
 # .config("spark.mongodb.connection.uri", pyspark_config.MONGODB_URI)
+# .config("spark.mongodb.input.uri", f"{pyspark_config.MONGODB_URI}{pyspark_config.DB_NAME}")
+# .config("spark.mongodb.output.uri", f"{pyspark_config.MONGODB_URI}{pyspark_config.DB_NAME}")
 spark_mongo = (
     SparkSession.builder.appName("Spark/Mongo IO")
     .master("spark://spark-master:7077")
     .config("spark.executor.memory", "2g")
+    .config("spark.mongodb.connection.uri", pyspark_config.MONGODB_URI)
     .config(
-        "spark.mongodb.read.connection.uri",
-        f"{pyspark_config.MONGODB_URI}{pyspark_config.DB_NAME}",
+        "spark.driver.extraJavaOptions",
+        "-Dlog4j.configuration=file:///opt/bitnami/logs/log4j.properties -Dlog4j.debug",
     )
     .config(
-        "spark.mongodb.write.connection.uri",
-        f"{pyspark_config.MONGODB_URI}{pyspark_config.DB_NAME}",
+        "spark.executor.extraJavaOptions",
+        "-Dlog4j.configuration=file:///opt/bitnami/logs/log4j.properties -Dlog4j.debug",
     )
     .config(
         "spark.jars.packages",
         "org.mongodb.spark:mongo-spark-connector_2.12:10.4.0",
     )
-    .config(
-        "spark.driver.extraClassPath",
-        "/opt/bitnami/spark/jars/mongo-java-driver-3.12.10.jar",
-    )
-    .config(
-        "spark.executor.extraClassPath",
-        "/opt/bitnami/spark/jars/mongo-java-driver-3.12.10.jar",
-    )
     .getOrCreate()
 )
+# .config(
+#     "spark.jars",
+#     "/opt/bitnami/spark/jars/mongo-spark-connector_2.12-10.4.0.jar",
+# )
+# .config("spark.driver.extraClassPath", "/opt/bitnami/spark/jars/mongo-spark-connector_2.12-10.4.0.jar")
+# .config("spark.executor.extraClassPath", "/opt/bitnami/spark/jars/mongo-spark-connector_2.12-10.4.0.jar")
+
+# .config(
+#     "spark.mongodb.read.connection.uri",
+#     f"{pyspark_config.MONGODB_URI}{pyspark_config.DB_NAME}",
+# )
+# .config(
+#     "spark.mongodb.write.connection.uri",
+#     f"{pyspark_config.MONGODB_URI}{pyspark_config.DB_NAME}",
+# )
+
+# .config(
+#     "spark.jars.packages",
+#     "org.mongodb.spark:mongo-spark-connector_2.12:10.4.0",
+# )
+# .config(
+#     "spark.driver.extraClassPath",
+#     "/opt/spark/jars/mongo-java-driver-3.12.10.jar",
+# )
+# .config(
+#     "spark.executor.extraClassPath",
+#     "/opt/spark/jars/mongo-java-driver-3.12.10.jar",
+# )
 
 spark_postgres = (
     SparkSession.builder.appName("Spark/Postgres IO")
