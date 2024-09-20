e8d17b2478fe7c4e8a391fb64b1ee0158444cb13
Pierre-Edouard Barrault <pebarrault@tuta.io>
2024-09-20 12:27:43 +0000

    volume and logs path

--- orchestrator/src/__init__.py
+++ orchestrator/src/__init__.py
@@ -1,3 +1,7 @@
+"""
+Logging module based on common logger
+"""
+
 from src.commons.logging import LoggerManager
 
-logger = LoggerManager(log_filepath="/opt/celery_app/logs/worker.log").get_logger()
+logger = LoggerManager(log_filepath="/opt/orchestrator/logs/worker.log").get_logger()
