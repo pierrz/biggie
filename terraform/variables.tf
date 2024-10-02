99ec48b7e7b407972133b1f53ed267143e41f7da
Pierre-Edouard Barrault <pebarrault@tuta.io>
2024-10-02 16:31:45 +0300

    terraform fmt

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,49 +1,49 @@
 variable "scaleway_access_key" {
-    description = "Scaleway access key"
-    type        = string
+  description = "Scaleway access key"
+  type        = string
 }
 
 variable "scaleway_secret_key" {
-    description = "Scaleway secret key"
-    type        = string
+  description = "Scaleway secret key"
+  type        = string
 }
 
 variable "scaleway_organization_id" {
-    description = "Scaleway organization ID"
-    type        = string
+  description = "Scaleway organization ID"
+  type        = string
 }
 
 variable "scaleway_project_id" {
-    description = "Scaleway project ID."
-    type        = string
+  description = "Scaleway project ID."
+  type        = string
 }
 
 variable "scaleway_zone" {
-    description = "Scaleway zone"
-    type        = string
+  description = "Scaleway zone"
+  type        = string
 }
 
 variable "scaleway_server_user" {
-    description = "The user used to connect to server"
-    type        = string
+  description = "The user used to connect to server"
+  type        = string
 }
 
 variable "scaleway_server_password" {
-    description = "The password for the user connecting to the server"
-    type        = string
+  description = "The password for the user connecting to the server"
+  type        = string
 }
 
 variable "scaleway_server_id" {
-    description = "The Scaleway server ID"
-    type        = string
+  description = "The Scaleway server ID"
+  type        = string
 }
 
 variable "scaleway_server_name" {
-    description = "The Scaleway server name"
-    type        = string
+  description = "The Scaleway server name"
+  type        = string
 }
 
 variable "scaleway_ssh_key_id" {
-    description = "The ssh key ID from the Scaleway server"
-    type        = string
+  description = "The ssh key ID from the Scaleway server"
+  type        = string
 }
