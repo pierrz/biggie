f260e9cb9c210e71694e38222c64e8a833392da6
Pierre-Edouard Barrault <pebarrault@tuta.io>
2024-10-05 20:35:52 +0300

    agent

--- terraform/main.tf
+++ terraform/main.tf
@@ -89,6 +89,7 @@ resource "null_resource" "server_configuration" {
       tsh ssh ${var.scaleway_server_user}@${var.scaleway_server_name} \
       '
         rm -rf /opt/biggie/*
+        eval "$(ssh-agent -s)"
         ssh-add /home/terraform-cd/.ssh/id_ed25519_github.pub
         git clone git@github.com:${var.github_repo_name}.git:${var.github_repo_branch} /opt/biggie
 
