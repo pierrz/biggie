d2e56076dbda13ef5f42da72ef76bbe1ac4de4d3
Pierre-Edouard Barrault <pebarrault@tuta.io>
2024-10-08 10:35:29 +0300

    ports and service names fixes for ra rabbitmq

--- orchestrator/run.sh
+++ orchestrator/run.sh
@@ -2,5 +2,5 @@
 
 celery --app=worker.celery beat --loglevel=info --logfile=logs/beat.log --detach
 
-# celery worker not detached / showing in docker-compose terminal
+# celery worker is not detached i.e. logs are visible in docker-compose terminal
 celery --app=worker.celery worker --loglevel=info --logfile=logs/worker.log -Q data_pipeline
