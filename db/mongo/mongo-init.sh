11f32a3f871d9075b0cc31fac4b5fb570a60b6a5
Pierre-Edouard Barrault <pebarrault@tuta.io>
2024-10-09 23:31:32 +0300

    beat debug

--- db/mongo/mongo-init.sh
+++ db/mongo/mongo-init.sh
@@ -12,21 +12,4 @@ mongosh admin -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" 
   })
 EOF
 
-# mongosh <<EOF
-# db.auth('$MONGO_INITDB_ROOT_USERNAME', '$MONGO_INITDB_ROOT_PASSWORD')
-
-# db = db.getSiblingDB('$DB_NAME')
-
-# db.createUser({
-#   user: '$MONGO_USERNAME',
-#   pwd: '$MONGO_PASSWORD',
-#   roles: [
-#     {
-#       role: 'readWrite',
-#       db: '$DB_NAME',
-#     },
-#   ],
-# })
-# EOF
-
 echo "MongoDB initialized."
--- orchestrator/run.sh
+++ orchestrator/run.sh
@@ -3,4 +3,4 @@
 celery --app=worker.celery beat --loglevel=info --logfile=logs/beat.log --detach
 
 # celery worker is not detached i.e. logs are visible in docker-compose terminal
-celery --app=worker.celery worker --loglevel=info --logfile=logs/worker.log -Q data_pipeline
+celery --app=worker.celery worker --loglevel=debug --logfile=logs/worker.log -Q data_pipeline
