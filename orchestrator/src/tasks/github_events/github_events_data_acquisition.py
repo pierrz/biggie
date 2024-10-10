64e631cea9b23f8587a6841233f66cd6221d89e3
Pierre-Edouard Barrault <pebarrault@tuta.io>
2024-10-10 19:03:43 +0300

    teleport + nginx + monitoring wip

--- orchestrator/src/tasks/github_events/github_events_data_acquisition.py
+++ orchestrator/src/tasks/github_events/github_events_data_acquisition.py
@@ -9,9 +9,9 @@ from typing import Union
 
 from config import data_directories
 from src import logger
-from src.harvester.asyncio_operations import download_github_events, write_aio
-from src.harvester.auth_parameters import github_params
-from src.harvester.github_events_urls import get_events_urls
+from src.harvester.commons.asyncio_operations import download_github_events, write_aio
+from src.harvester.github_events.auth_parameters import github_params
+from src.harvester.github_events.github_events_urls import get_events_urls
 from worker import celery
 
 
