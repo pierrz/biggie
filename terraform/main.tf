e4751b90dd50719668b9285e89616d2ec177bed6
Pierre-Edouard Barrault <pebarrault@tuta.io>
2024-10-16 12:01:00 +0300

    wip hash

--- terraform/main.tf
+++ terraform/main.tf
@@ -94,6 +94,7 @@ resource "null_resource" "compose_setup" {
         cd ~
         source biggie-cd-venv/bin/activate
         HASHED_PASSWORD=$(python -c "from jupyter_server.auth import passwd; print(passwd('${var.jupyter_password}'))")
+        echo "$HASHED_PASSWORD"
 
         echo "Creating .env file ..."
         cd /opt/biggie
